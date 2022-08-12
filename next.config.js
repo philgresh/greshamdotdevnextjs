a261e7bb1a352c3b03eb5ce97893d8f9a3a239cf
Phil Gresham <phil@gresham.dev>
2022-08-11 20:06:04 -0700

    Init from create-next-app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js',
-  unstable_staticImage: true,
-})
-module.exports = withNextra()
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
